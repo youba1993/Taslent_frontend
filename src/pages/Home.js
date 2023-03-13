7853f2160a2180cf4c033226b9ce7a70e401912a
youba idjis <youba.etud@gmail.com>
2023-03-13 00:05:39 -0700

    Setting React-bootstrap

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header";
+
+const Home = () => {
+    return (
+        <body>
+            <header>
+                <Header />
+            </header>
+        </body>
+    );
+}
+
+export default Home;
