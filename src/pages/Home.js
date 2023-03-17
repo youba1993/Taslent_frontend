d90175899313b05cedef3bbbaa6a3d791520f7ee
youba idjis <youba.etud@gmail.com>
2023-03-16 19:50:45 -0700

    Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,35 @@
+import { Col, Container, Row } from "react-bootstrap";
 import CarouselPage from "../components/Carrousel";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
 
 
+
 const Home = () => {
     return (
         <div>
-           
-                <Header />
-               
-                <CarouselPage />
-         
-                
 
-           
-                <Footer />
-            
+            <Header />
+            <CarouselPage />
+            <Container className="m-5" >
+                <Row className='bg-light text-center me-auto'>
+                    <Col>
+                        <h3 >Unity</h3>
+                    </Col>
+                    <Col>
+                        <h3>knowledge</h3>
+                    </Col>
+                    <Col>
+                        <h3>Respect</h3>
+                    </Col>
+                </Row>
+            </Container>
+
+
+
+            <Footer />
+
         </div>
     );
 }
