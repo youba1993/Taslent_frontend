9f71f15f493d572c89c4e85196001cdc5b4cb2d6
youba idjis <youba.etud@gmail.com>
2023-03-21 00:18:52 -0700

    Projects page

--- src/components/ProjectCom.js
+++ src/components/ProjectCom.js
@@ -1,8 +1,24 @@
+import { Container } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+
+
 const ProjectCom = () => {
-    return ( 
-        <>
-        </>
-     );
+    return (
+        <Container >
+            <Card style={{ width: '23rem' }}>
+                <Card.Img variant="top" src="https://th.bing.com/th/id/OIP.tw74t63fT3e7NHveaPvTxAHaE7?pid=ImgDet&w=2000&h=1333&rs=1" />
+                <Card.Body>
+                    <Card.Title>Project title</Card.Title>
+                    <Card.Text>
+                        This is the description of the project and the requirments.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">this is the difficulty of the project</small>
+                </Card.Footer>
+            </Card>
+        </Container>
+    );
 }
- 
+
 export default ProjectCom;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,29 @@
 import ProjectCom from "../components/ProjectCom";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { Container, Nav } from "react-bootstrap";
 
 const Projects = () => {
-    return ( 
-        <>
-        <ProjectCom />
-        </>
-     );
+    return (
+        <Container>
+            <Row className="g-4">
+
+                <Nav id="sidebar">
+                   
+                        <h3>Category:</h3>
+                    
+                    
+                </Nav>
+
+                {Array.from({ length: 10 }).map((_, idx) => (
+                    <Col >
+                        <ProjectCom />
+                    </Col>
+                ))}
+            </Row>
+
+        </Container>
+    );
 }
- 
+
 export default Projects;
