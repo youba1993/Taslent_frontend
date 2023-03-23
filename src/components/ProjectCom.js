98d862247f4b1436010f034fb71b424e3d625927
youba idjis <youba.etud@gmail.com>
2023-03-22 23:50:19 -0700

    project likes and comment

--- src/components/ProjectCom.js
+++ src/components/ProjectCom.js
@@ -1,23 +1,36 @@
 import Card from 'react-bootstrap/Card';
-
+import { AiOutlineComment, AiOutlineLike } from "react-icons/ai";
 
 const ProjectCom = (project) => {
-    const {title, image, description, difficulty} = project.project
+    const { title, image, description, difficulty } = project.project
 
     return (
-        
-            <Card  style={{ width: '20rem' }}>
-                <Card.Img  variant="top" src={image} />
-                <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>
-                        {description}
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">difficulty of the project {difficulty}</small>
-                </Card.Footer>
-            </Card>
+
+        <Card style={{ width: '20rem' }}>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {description}
+                    <hr/>
+                    <p>{difficulty} project</p>
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <div className="d-flex justify-content-start">
+                    <div className='d-flex align-items-center me-3'>
+                        <i className="me-2"><AiOutlineLike /></i>
+                        <p class="mb-0 small">Like</p>
+                    </div>
+                    <div className='d-flex align-items-center me-3'>
+                        <i className="me-2"><AiOutlineComment /></i>
+                        <p class="mb-0 small">Comment</p>
+                    </div>
+                    
+                </div>
+
+            </Card.Footer>
+        </Card>
     );
 }
 
