0f8678085398d9dc0df2f8c505aeb8e39675452b
youba idjis <youba.etud@gmail.com>
2023-03-23 17:50:05 -0700

    Projects and gallery

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -6,9 +6,11 @@ const Gallery = () => {
 
         <Container className="py-5">
             <h1 className="text-center mb-5">Gallery</h1>
-            <p className="lead text-center pb-4">Welcome to our gallery, where we showcase some of the beautiful sights and events of our village.</p>
-
             <CarouselPage />
+            
+            <p className="lead text-center py-4">Welcome to our gallery, where we showcase some of the beautiful sights and events of our village.</p>
+
+            
 
         </Container>
     );
