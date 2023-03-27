13f15a0ba70ddd51492631bd0c34afd1035f9488
youba idjis <youba.etud@gmail.com>
2023-03-26 23:34:54 -0700

    setting firebase auth

---
+++ src/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCn-V9U_S_JMYQvmZsAPhXnkcx1Ba6DxBU",
+  authDomain: "taslent-73e17.firebaseapp.com",
+  projectId: "taslent-73e17",
+  storageBucket: "taslent-73e17.appspot.com",
+  messagingSenderId: "169199534374",
+  appId: "1:169199534374:web:0bf5acaff40ef65a2741e7",
+  measurementId: "G-Z73M5600ER"
+};
+
+// Initialize Firebase
+export const appFirebase = initializeApp(firebaseConfig);
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import { appFirebase } from "../firebaseConfig";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// fire base auth
+// import firebase from 'firebase/compat/app';
+// import * as firebaseui from 'firebaseui'
+// import 'firebaseui/dist/firebaseui.css'
 
 const Login = ({ show, handleClose }) => {
     const [activeTab, setActiveTab] = useState("login");
+    const [data, setData ] = useState({});
+    let auth = getAuth();
+
+   const  handleInput = (event) => {
+        let newInput = { [event.target.name]: event.target.value};
+        setData({ ...data, ...newInput})
+    }
 
     const handleTabChange = (tab) => {
         setActiveTab(tab);
     };
 
     return (
-        <Modal show={show} onHide={()=>handleClose()}>
+        <Modal show={show} onHide={() => handleClose()}>
             <Modal.Header closeButton>
                 <Modal.Title>{activeTab === "login" ? "Login" : "Signup"}</Modal.Title>
             </Modal.Header>
@@ -19,24 +32,24 @@ const Login = ({ show, handleClose }) => {
                         <div>
                             <Form.Group controlId="formBasicFirstName">
                                 <Form.Label>First Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter first name" />
+                                <Form.Control name="firstName" type="text" placeholder="Enter first name" onChange={(e)=>handleInput(e)}/>
                             </Form.Group>
 
                             <Form.Group controlId="formBasicLastName">
                                 <Form.Label>Last Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter last name" />
+                                <Form.Control name="lastName" type="text" placeholder="Enter last name" onChange={(e)=>handleInput(e)} />
                             </Form.Group>
                         </div>
                     )}
 
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control name="email" type="email" placeholder="Enter email" onChange={(e)=>handleInput(e)} />
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control name="password" type="password" placeholder="Password" onChange={(e)=>handleInput(e)}/>
                     </Form.Group>
 
                     {activeTab === "login" && (
@@ -50,7 +63,7 @@ const Login = ({ show, handleClose }) => {
                 <Button variant="secondary" onClick={handleClose}>
                     Close
                 </Button>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" >
                     {activeTab === "login" ? "Login" : "Signup"}
                 </Button>
             </Modal.Footer>
